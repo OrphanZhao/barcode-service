35f02064b7cc961f9cb6882d9ccea8606c98c772
class moon <57883335+OrphanZhao@users.noreply.github.com>
2024-11-01 15:20:21 +0800

    chore: v2

--- app.js
+++ app.js
@@ -1,57 +1,100 @@
-const express = require("express");
-const puppeteerService = require("./PuppeteerService");
+const fs = require("fs");
+const path = require("path");
 const dotenv = require("dotenv");
+const puppeteer = require("puppeteer");
 const logger = require("./logger");
-require("express-async-errors");
 
 dotenv.config();
+const token = process.env.TOKEN;
+const baseUrl = "https://bff.gds.org.cn/gds/searching-api/ProductService";
+const defaultBarcode = 6932203601439;
 
-const app = express();
-const port = process.env.PORT;
+const main = async () => {
+  const startBrowser = async () => {
+    const browser = await puppeteer.launch({ headless: true });
+    const page = await browser.newPage();
+    page.setDefaultNavigationTimeout(60000);
+    await page.goto("https://www.gds.org.cn/#/home/index", {
+      waitUntil: "domcontentloaded",
+    });
+    return page;
+  };
 
-app.use(express.json());
-
-let token = process.env.TOKEN;
-
-// 初始化 PuppeteerService
-(async () => {
-  try {
-    await puppeteerService.initialize();
-  } catch (error) {
-    logger.error("Failed to initialize PuppeteerService:", error);
-  }
-})();
-
-app.get("/barcode/:id", async (req, res) => {
-  const { id: barcode } = req.params;
-  if (!barcode) {
-    return res.status(400).json({ error: "Barcode is required" });
-  }
-
-  logger.info("Fetching barcode detail", { barcode });
-
-  try {
-    const infoResponse = await puppeteerService.fetchBarcodeDetail(
+  const productListByGtin = async (page, barcode = defaultBarcode) => {
+    return await page.evaluate(
+      async (barcode, baseUrl, token) => {
+        const response = await fetch(
+          `${baseUrl}/ProductListByGTIN?PageSize=30&PageIndex=1&SearchItem=${barcode}`,
+          { headers: { Authorization: `Bearer ${token}` } }
+        );
+        return response.json();
+      },
       barcode,
+      baseUrl,
       token
     );
-    logger.info("Barcode detail fetched successfully", {
-      barcode,
-      infoResponse,
-    });
-    res.json({ success: true, data: infoResponse });
-  } catch (error) {
-    logger.error("请求失败:", error.message, { errorResponse: error.response });
-    res.status(500).json({ success: false, error: error.message });
-  }
-});
+  };
 
-// 错误处理中间件
-app.use((err, req, res, next) => {
-  logger.error("Unhandled error:", err);
-  res.status(500).json({ success: false, error: "Internal Server Error" });
-});
+  const productListByFid = async (page, f_id) => {
+    return await page.evaluate(
+      async (f_id, baseUrl, token) => {
+        const response = await fetch(
+          `${baseUrl}/ProductListByFID?PageSize=500&PageIndex=1&SearchItem=${f_id}`,
+          { headers: { Authorization: `Bearer ${token}` } }
+        );
+        return response.json();
+      },
+      f_id,
+      baseUrl,
+      token
+    );
+  };
 
-app.listen(port, () => {
-  logger.info(`Server running at http://localhost:${port}`);
-});
+  const productInfoByGtin = async (page, gtin, base_id) => {
+    return await page.evaluate(
+      async (gtin, base_id, baseUrl, token) => {
+        const response = await fetch(
+          `${baseUrl}/ProductInfoByGTIN?gtin=${gtin}&id=${base_id}`,
+          { headers: { Authorization: `Bearer ${token}` } }
+        );
+        return response.json();
+      },
+      gtin,
+      base_id,
+      baseUrl,
+      token
+    );
+  };
+
+  const start = async () => {
+    const page = await startBrowser();
+
+    const {
+      Data: { Items },
+    } = await productListByGtin(page);
+    const [target] = Items;
+    const { f_id } = target;
+
+    const {
+      Data: { Items: all },
+    } = await productListByFid(page, f_id);
+
+    for (const item of all) {
+      await new Promise(async (resolve) => {
+        console.log(item);
+        let res = {};
+        try {
+          res = await productInfoByGtin(page, item.gtin, item.base_id);
+        } catch (error) {
+          console.log(error);
+          res = error;
+        }
+        logger.info("productInfoByGtin", { res });
+        setTimeout(resolve, 5000);
+      });
+    }
+  };
+  start();
+};
+
+main();
